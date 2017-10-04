2d7cbe54fca7519bbe4ce5b70ac2c5a41f56ab99
Guillaume QUERRE <gquerre@student.42.fr>
2017-10-04 06:06:42 +0200

    test

--- ft_percent.c
+++ ft_percent.c
@@ -6,7 +6,7 @@
 /*   By: gquerre <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/27 06:18:27 by gquerre           #+#    #+#             */
-/*   Updated: 2017/10/04 06:01:09 by gquerre          ###   ########.fr       */
+/*   Updated: 2017/10/04 06:06:36 by gquerre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,5 @@ int		ft_percent(t_env *e)
 	add[1] = '\0';
 	if (!(e->stock = ft_strjoinfree(e->stock, add)))
 		return (-1);
-	return (e->size_arg + 1);
+	return (2);
 }
