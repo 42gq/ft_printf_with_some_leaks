fad6b43104c0a137fc3685775f41ac6a1dffa8ec
Guillaume QUERRE <gquerre@student.42.fr>
2017-10-05 07:30:28 +0200

    test

--- handlers/ft_handle_wchar.c
+++ handlers/ft_handle_wchar.c
@@ -6,7 +6,7 @@
 /*   By: gquerre <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/28 06:17:32 by gquerre           #+#    #+#             */
-/*   Updated: 2017/10/05 07:27:57 by gquerre          ###   ########.fr       */
+/*   Updated: 2017/10/05 07:30:14 by gquerre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,9 @@ int	ft_handle_wchar(wint_t v_arg, t_env *e)
 	add = NULL;
 	chr = v_arg;
 	if (chr > 128 && chr < 255)
-		chr = (char)chr;
-	add = ft_conv_wint_to_str(chr, e);
+		ft_handle_char((char)chr);
+	else
+		add = ft_conv_wint_to_str(chr, e);
 	if (e->preci_size && e->size_w > e->preci)
 		ft_strclr(add);
 	mem = ft_apply_optionschar(add, e);
