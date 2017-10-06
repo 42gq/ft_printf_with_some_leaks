d70990b3e960620549b0520ab9e6d161e7d35977
Guillaume QUERRE <gquerre@student.42.fr>
2017-10-06 07:57:37 +0200

    Addyolo

--- ft_error_printf.c
+++ ft_error_printf.c
@@ -6,7 +6,7 @@
 /*   By: gquerre <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/03 05:21:12 by gquerre           #+#    #+#             */
-/*   Updated: 2017/10/05 05:58:04 by gquerre          ###   ########.fr       */
+/*   Updated: 2017/10/06 07:51:20 by gquerre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ void	ft_write_error(int i)
 	if (i == 6)
 		ft_putstr("- Options != somme_options, check ft_condition\n");
 	if (i == 7)
-		ft_putstr("- BITE\n");
+		ft_putstr("");
 }
 
 int		ft_error_printf(int i)
